ed0569e41c50dc886c4895aa2ca80667924c38de
harshita <harshita@logicrays.com>
2024-03-27 11:33:43 +0530

    27mar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import FirstComopent from './components/FirstComponent';
 
 function App() {
+  let a=10
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className='App'>
+      <h1 style={{color:'red',backgroundColor:'yellow'}}>Hello React</h1>
+      <h3 className='text-primary' style={{background:'aqua'}}>javascript variable: {a}</h3>
+
+      <FirstComopent></FirstComopent>
+      <FirstComopent/>
+      <hr/>
+        <button
+          type="button"
+          class="btn btn-primary"
         >
-          Learn React
-        </a>
-      </header>
+          Button
+        </button>
+        
+
     </div>
   );
 }
 
-export default App;
+export default App
---
+++ src/components/FirstComponent.jsx
@@ -0,0 +1,35 @@
+// function FirstComopent(){
+//     return(
+//         <h1>First Comp.</h1>
+//     )
+// }
+
+// export default FirstComopent
+
+//rfce
+// import React from 'react'
+
+// function FirstComponent() {
+//   return (
+//     <div>
+//<h1>First Comp.</h1> 
+//     </div>
+//   )
+// }
+
+// export default FirstComponent
+
+//rafc
+import React from 'react'
+
+const FirstComponent = () => {
+    let a=10
+  return (
+    <div>
+       <h1>First Comp.</h1>
+       {/* {a} */}
+    </div>
+  )
+}
+
+export default FirstComponent
